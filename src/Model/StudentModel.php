64d4e4bbdde5ffe7501a4bc82b9d281d30eb1ecc
canoy1178 <jpcanoy5@gmail.com>
2025-07-28 17:51:34 +0800

    up

--- src/Model/StudentModel.php
+++ src/Model/StudentModel.php
@@ -1,15 +1,18 @@
 <?php
 namespace Canoy\StudentManagement\Model;
 use Canoy\StudentManagement\Core\Crud;
+use Canoy\StudentManagement\Core\Database;
 
-class StudentModel implements Crud {
+Class StudentModel extends Database implements Crud {
     public $id;
     public $fullname;
     public $yearlevel;
     public $course;
     public $section;
 
-    public function __construct(){
+    public function __construct()
+    { 
+        parent::__construct();
         $this->id = "";
         $this->fullname = "";
         $this->yearlevel = "";
@@ -18,23 +21,65 @@ class StudentModel implements Crud {
     }
 
     public function displayInfo(){
-        echo "id: " . $this->id."\n";
+        echo "Id: " . $this->id."\n";
         echo "Name: " . $this->fullname."\n";
-        echo "Year Level: " . $this->yearlevel."\n";
-        echo "course: " . $this->course."\n";
-        echo "section: " . $this->section."\n";
+        echo "Year Level: " . $this->yearLevel."\n";
+        echo "Course: " . $this->course."\n";
+        echo "Section: " . $this->section."\n";
     }
 
     public function create(){
-     
+        try {
+            $sql = "INSERT INTO students (id, fullname, yearLevel, course, section) VALUES (?, ?, ?, ?, ?)";
+            $stmt = $this->conn->prepare($sql);
+            $stmt-> bind_param("ssss", $this->id, $this->fullname, $this->yearLevel, $this->course, $this->section);
+            if ($stmt->execute()) {
+                return true;
+            }else{
+                return false;
+            }
+        }catch(\Exception $e){
+            echo $e->getMessage();
+            return false;
+        }
     }
     public function read(){
-
+        try{
+            $sql = "SELECT * FROM students";
+            $results = $this->conn->query($sql);             
+            return $results->fetch_all(MYSQLI_ASSOC);
+        }catch(\Exception $e){
+            echo $e->getMessage();
+        }
     }
     public function update(){
-
+        try{
+            $sql = "UPDATE students SET fullname=?, yearLevel=?, course=?, section=? WHERE id=?";
+            $stmt = $this->conn->prepare($sql);
+            $stmt-> bind_param("ssss", $this->fullname, $this->yearLevel, $this->course, $this->section, $this->id,);
+            if ($stmt->execute()) {
+                return true;
+            }else{
+                return false;
+            }
+        }catch(\Exception $e){
+            echo $e->getMessage();
+            return false;
+        }
     }
     public function delete(){
-
+        try {
+            $sql = "DELETE FROM students WHERE id=?";
+            $stmt = $this->conn->prepare($sql);
+            $stmt-> bind_param("i", $this->id,);
+            if ($stmt->execute()) {
+                return true;
+            }else{
+                return false;
+            }
+        }catch(\Exception $e){
+            echo $e->getMessage();
+            return false;
+        }
     }
 }
