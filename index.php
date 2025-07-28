b076d2d6e917eab2616be4db7b1f820386258aec
canoy1178 <jpcanoy5@gmail.com>
2025-07-28 17:50:37 +0800

    up

--- index.php
+++ index.php
@@ -3,11 +3,3 @@ include 'vendor/autoload.php';
 
 use Canoy\StudentManagement\Model\StudentModel;
 
-$student = new StudentModel;
-$student->id = 2024624001;
-$student->fullname = "John Paul Canoy";
-$student->yearlevel = "First Year";
-$student->course = "BSIT";
-$student->section = "A";
-
-$student->displayInfo();
